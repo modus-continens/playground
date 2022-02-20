59add8bc740f0f615bb272291aa9a7ab6431eeab
Mao <m@maowtm.org>
2022-02-20 22:00:40 +0000

    modus-lib rename

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 use codespan_reporting::term;
-use modus_corelib::{
+use modus_lib::{
     analysis::check_and_output_analysis,
     modusfile::{Expression, Modusfile},
     sld::{self, tree_from_modusfile},
@@ -31,65 +31,78 @@ impl ModusResult {
 
 #[wasm_bindgen]
 pub fn get_proof_tree(mf_source: &str, goal: &str) -> ModusResult {
-    let mf: Modusfile = match mf_source.parse() {
-        Ok(mf) => mf,
-        Err(e) => {
-            return ModusResult {
-                success: false,
-                errors: format!("Unable to parse input:\n{}", e),
-                proofs: vec![],
-            };
-        }
-    };
-    let query: Expression = match goal.parse() {
-        Ok(q) => q,
-        Err(e) => {
-            return ModusResult {
-                success: false,
-                errors: format!("Unable to parse query:\n{}", e),
-                proofs: vec![],
-            };
-        }
-    };
-    let query = query.without_position();
-
-    let f = codespan_reporting::files::SimpleFile::new("Modusfile", mf_source);
-
-    let mut analysis_err_buf = termcolor::Buffer::no_color();
-    if !check_and_output_analysis(&mf, false, &mut analysis_err_buf, &Default::default(), &f) {
-        return ModusResult {
-            success: false,
-            errors: std::str::from_utf8(analysis_err_buf.as_slice())
-                .unwrap()
-                .to_owned(),
-            proofs: vec![],
+    let res = std::panic::catch_unwind(move || {
+        let mf: Modusfile = match mf_source.parse() {
+            Ok(mf) => mf,
+            Err(e) => {
+                return ModusResult {
+                    success: false,
+                    errors: format!("Unable to parse input:\n{}", e),
+                    proofs: vec![],
+                };
+            }
         };
-    }
+        let query: Expression = match goal.parse() {
+            Ok(q) => q,
+            Err(e) => {
+                return ModusResult {
+                    success: false,
+                    errors: format!("Unable to parse query:\n{}", e),
+                    proofs: vec![],
+                };
+            }
+        };
+        let query = query.without_position();
 
-    let max_depth = 175;
-    let (goal, clauses, sld_result) = tree_from_modusfile(mf, query.clone(), max_depth, true);
+        let f = codespan_reporting::files::SimpleFile::new("Modusfile", mf_source);
 
-    let proof_result = Result::from(sld_result).map(|t| sld::proofs(&t, &clauses, &goal));
-    let mut proofs = Vec::new();
-    match proof_result {
-        Ok(ps) => {
-            for (_, proof) in ps {
-                let t = proof.get_tree(&clauses);
-                let mut tstr: Vec<u8> = Vec::new();
-                ptree::write_tree(&t, &mut tstr).unwrap();
-                proofs.push(String::from_utf8_lossy(&tstr).into_owned());
+        let mut analysis_err_buf = termcolor::Buffer::no_color();
+        if !check_and_output_analysis(&mf, false, &mut analysis_err_buf, &Default::default(), &f) {
+            return ModusResult {
+                success: false,
+                errors: std::str::from_utf8(analysis_err_buf.as_slice())
+                    .unwrap()
+                    .to_owned(),
+                proofs: vec![],
+            };
+        }
+
+        let max_depth = 175;
+        let (goal, clauses, sld_result) = tree_from_modusfile(mf, query.clone(), max_depth, true);
+
+        let proof_result = Result::from(sld_result).map(|t| sld::proofs(&t, &clauses, &goal));
+        let mut proofs = Vec::new();
+        match proof_result {
+            Ok(ps) => {
+                for (_, proof) in ps {
+                    let t = proof.get_tree(&clauses);
+                    let mut tstr: Vec<u8> = Vec::new();
+                    ptree::write_tree(&t, &mut tstr).unwrap();
+                    proofs.push(String::from_utf8_lossy(&tstr).into_owned());
+                }
+            }
+            Err(e) => {
+                for diag_error in e {
+                    term::emit(&mut analysis_err_buf, &Default::default(), &f, &diag_error).unwrap();
+                }
             }
         }
+
+        return ModusResult {
+            success: true,
+            proofs,
+            errors: String::from_utf8_lossy(analysis_err_buf.as_slice()).into_owned(),
+        };
+    });
+
+    match res {
+        Ok(r) => r,
         Err(e) => {
-            for diag_error in e {
-                term::emit(&mut analysis_err_buf, &Default::default(), &f, &diag_error).unwrap();
+            ModusResult {
+                success: false,
+                proofs: vec![],
+                errors: format!("Modus panicked: {:?}\nPlease report this at https://github.com/modus-continens/modus/issues", e),
             }
         }
     }
-
-    return ModusResult {
-        success: true,
-        proofs,
-        errors: String::from_utf8_lossy(analysis_err_buf.as_slice()).into_owned(),
-    };
 }
